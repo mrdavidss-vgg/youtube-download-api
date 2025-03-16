72d52a81f48db416a90f117dcb5031ea585a67d9
David <mysteryzonmusic@gmail.com>
2025-03-16 11:38:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { exec } = require('child_process');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Endpoint to fetch the download URL
 app.get('/download', (req, res) => {
